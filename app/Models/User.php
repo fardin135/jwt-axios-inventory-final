df47d5d58cc9b3a1a41bab12b22621620658f900
fardin <fardinfaiaz5@gmail.com>
2025-03-13 01:54:09 +0600

    added JWT token

---
+++ app/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = ['firstName', 'lastName', 'email', 'mobile', 'password', 'otp'];
+
+    protected $attributes = [
+        'otp' => '0'
+    ];
+}
